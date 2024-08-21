2a9b8d062c5db338defd3ef18050ff627f895bf0
ksnowmanu <k.snowman.u@gmail.com>
2024-08-21 09:53:50 +0900

    Xicon

--- CNS-nextui-app/config/site.ts
+++ CNS-nextui-app/config/site.ts
@@ -61,7 +61,7 @@ export const siteConfig = {
   ],
   links: {
     github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
+    twitterX: "https://x.com/Grim_shop",
     docs: "https://nextui.org",
     discord: "https://discord.gg/9b6yyZKmH4",
     sponsor: "https://patreon.com/jrgarciadev",
