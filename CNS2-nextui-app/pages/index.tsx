62975e5b0dbe2e07ec875e47b0e045b8785f1db7
ksnowmanu <k.snowman.u@gmail.com>
2024-08-26 23:54:27 +0900

    test

--- CNS2-nextui-app/pages/index.tsx
+++ CNS2-nextui-app/pages/index.tsx
@@ -2,6 +2,9 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { Button } from "@nextui-org/button"
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
@@ -11,6 +14,90 @@ import DefaultLayout from "@/layouts/default";
 export default function IndexPage() {
   return (
     <DefaultLayout>
+      <section className="flex flex-row items-center justify-center gap-4 p-0 h-[30rem]">
+        <div className="flex-row bg-[url('/network_image.jpg')] bg-cover bg-center h-full w-full">
+
+          <div className="w-full h-full gap-6 grid grid-cols-12 grid-rows-2 p-8">
+
+            <Card className="col-span-12 sm:col-start-2 sm:col-span-3 h-[200px]">
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+                <h4 className="text-white/90 font-medium text-xl">Trading Cards</h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Woman listing to music"
+                className="z-0 w-full h-full scale-100 translate-y-0 object-cover"
+                src="trc.jpg"
+              />
+              <CardFooter className="justify-between bg-black/40 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p className="text-tiny text-white/80">Available soon.</p>
+                <Button className="text-tiny text-white bg-black/60" variant="flat" color="default" radius="lg" size="sm">
+                  Notify me
+                </Button>
+              </CardFooter>
+            </Card>
+            
+            <Card isFooterBlurred className="w-full h-[200px] col-span-12 sm:col-span-3">
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+                <h4 className="text-white/90 font-medium text-xl">illustration</h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Woman listing to music"
+                className="z-0 w-full h-full scale-100 translate-y-0 object-cover"
+                src="sumple_ill2.png"
+              />
+              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p className="text-tiny text-white/80">Available soon.</p>
+                <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+                  Notify me
+                </Button>
+              </CardFooter>
+            </Card>
+
+            <Card isFooterBlurred className="w-full h-[200px] col-span-12 sm:col-start-6 sm:col-span-3">
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+                <h4 className="text-white font-medium text-2xl">Photograph</h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                src="https://nextui.org/images/card-example-6.jpeg"
+              />
+              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p className="text-tiny text-white/80">Available soon.</p>
+                <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+                  Notify me
+                </Button>
+              </CardFooter>
+            </Card>
+
+            <Card isFooterBlurred className="w-full h-[200px] col-span-12 sm:col-span-3">
+              <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+                <h4 className="text-white/90 font-medium text-xl">Comming Soon...</h4>
+              </CardHeader>
+              <Image
+                removeWrapper
+                alt="Relaxing app background"
+                className="z-0 w-full h-full object-cover"
+                src="https://nextui.org/images/card-example-5.jpeg"
+              />
+              <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                <p className="text-tiny text-white/80">Available soon.</p>
+                <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+                  Notify me
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+
+        </div>
+      </section>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-xl text-center justify-center">
           <h1 className={title()}>Make&nbsp;</h1>
